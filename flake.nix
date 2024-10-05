c76b5534a901308217f1e3aa2ef80f4aa5ee7c07
Jared Tobin <jared@jtobin.io>
2024-10-05 07:41:48 +0400

    lib: add ppad-hmac-drbg dep

--- flake.nix
+++ flake.nix
@@ -7,11 +7,16 @@
       url  = "git://git.ppad.tech/sha256.git";
       ref  = "master";
     };
+    ppad-hmac-drbg = {
+      type = "git";
+      url  = "git://git.ppad.tech/hmac-drbg.git";
+      ref  = "master";
+    };
     flake-utils.follows = "ppad-sha256/flake-utils";
     nixpkgs.follows = "ppad-sha256/nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ppad-sha256 }:
+  outputs = { self, nixpkgs, flake-utils, ppad-sha256, ppad-hmac-drbg }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         lib = "ppad-secp256k1";
@@ -20,11 +25,14 @@
         hlib = pkgs.haskell.lib;
 
         sha256 = ppad-sha256.packages.${system}.default;
+        hmac-drbg = ppad-hmac-drbg.packages.${system}.default;
 
         hpkgs = pkgs.haskell.packages.ghc981.extend (new: old: {
-          ppad-sha256 = ppad-sha256.packages.${system}.default;
+          ppad-sha256 = sha256;
+          ppad-hmac-drbg = hmac-drbg;
           ${lib} = new.callCabal2nix lib ./. {
             ppad-sha256 = new.ppad-sha256;
+            ppad-hmac-drbg = new.ppad-hmac-drbg;
           };
         });
 
